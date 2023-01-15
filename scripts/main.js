7a49162224766274cdcfa86dc2f57064372e3fdf
farah-st <farah.perezcarrillo@gmail.com>
2023-01-14 17:11:26 -0700

    CHANGES

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,7 @@
+let date = document.lastModified;
+let lastModifiedDate = new Date(date);
+document.querySelector(".updated").innerHTML = `Last updated: ${lastModifiedDate}`;
+
 let date = new Date();
 let lastUpdate = new Date(document.lastModified);
 document.querySelector(".lastUpdate").innerHTML = `Last updated: ${lastUpdate}`;
