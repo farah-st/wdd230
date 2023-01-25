a8a11ec34a1f639db54f865db9dd7f04bd94399c
farah-st <farah.perezcarrillo@gmail.com>
2023-01-24 18:47:32 -0700

    changes

--- lesson04/week4.js
+++ lesson04/week4.js
@@ -1,6 +1,7 @@
 const button = document.querySelector(".btn");
 function show(){
-    console.log("Function is working");
+    // console.log("Function is working");
     button.classList.add("newstyle");
+    // console.log("Function is working");
 }
 button.addEventListener("click", show);
