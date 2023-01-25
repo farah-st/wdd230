d5f8317f03649d16429fd777816b674fb2ad5b70
farah-st <farah.perezcarrillo@gmail.com>
2023-01-25 16:04:35 -0700

    class

--- lesson04/week4.js
+++ lesson04/week4.js
@@ -1,7 +1,16 @@
 const button = document.querySelector(".btn");
 function show(){
-    // console.log("Function is working");
-    button.classList.add("newstyle");
-    // console.log("Function is working");
+    // button.classList.add("newstyle");
+    button.classList.toggle("newstyle");
+
 }
 button.addEventListener("click", show);
+
+//Hamburger meny button JS
+const mainNav = document.querySelector(".navigation");
+const hamButton = document.querySelector(".ham");
+//when we press this button we want this to happend () function
+hamButton.addEventListener('click', ()=> {
+    mainNav.classList.toggle('responsive');
+
+}) 
