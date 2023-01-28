e234c38a2fb20ae4d4feede796d80e463187a016
farah-st <farah.perezcarrillo@gmail.com>
2023-01-27 17:00:29 -0700

    small css

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -1,6 +1,24 @@
-const mainNav = document.querySelector(".navigation");
-const hamButton = document.querySelector(".ham");
-hamButton.addEventListener('click', ()=> {
-    mainNav.classList.toggle('responsive');
+const date1 = document.querySelector("#date1");
+const message = document.querySelector("#emessage");
 
-}) 
+try {
+	const options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+		year: "numeric"
+	};
+	date1.innerHTML = `${new Date().toLocaleDateString("en-US", options)}</span>`;
+} catch (e) {
+	console.log("Error with code or your browser does not support Locale");
+}
+
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+const x=document.getElementById("hamburgerBtn");
+x.onclick=toggleMenu;
+
+let lastUpdate = document.lastModified;
+document.querySelector("#lastUpdate").innerHTML = `Last modification: ${lastUpdate}`;
