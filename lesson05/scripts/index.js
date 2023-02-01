fbde7780a29d19803f78e55a2814444d0755cd83
farah-st <farah.perezcarrillo@gmail.com>
2023-02-01 12:46:17 -0700

    assignment

---
+++ lesson05/scripts/index.js
@@ -0,0 +1,29 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+  const myItem = input.value;
+  input.value = '';
+
+  const listItem = document.createElement('li');
+  const listText = document.createElement('span');
+  const listBtn = document.createElement('button');
+
+  listItem.appendChild(listText);
+  listText.textContent = myItem;
+  listItem.appendChild(listBtn);
+  listBtn.textContent = 'X';
+  list.appendChild(listItem);
+
+  listBtn.addEventListener('click', () => {
+    list.removeChild(listItem);
+  });
+
+  input.focus();
+});
+
+input.addEventListener('button',function test(e){
+    if(input.value.length==25 || e.key=='.')
+        e.preventDefault();
+});
