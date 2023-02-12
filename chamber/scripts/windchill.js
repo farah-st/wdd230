18491665c070b795a7f067a91c8fb26d5d211efc
farah-st <farah.perezcarrillo@gmail.com>
2023-02-11 20:21:19 -0700

    windchill

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,11 +1,10 @@
-const temp = document.querySelector('.t').textContent;
 
-const windspeed = document.querySelector('.ws').textContent;
+const temp = document.querySelector("#temperature").textContent;
+const windspeed = document.querySelector("#windSpeed").textContent;
+let windchill = document.querySelector("#windChill");
+
 let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
-
-let windchill = document.querySelector('.wc');
 
 if (temp <= 50 && windspeed >= 3){
 	windchill.textContent = chill + ' mph';
 }
-
