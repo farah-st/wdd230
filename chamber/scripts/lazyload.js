e189b173cef172b40e2ac88eb9873736deaf129a
farah-st <farah.perezcarrillo@gmail.com>
2023-02-21 18:00:22 -0700

    changes

--- chamber/scripts/lazyload.js
+++ chamber/scripts/lazyload.js
@@ -1,29 +1,36 @@
-const images = document.querySelectorAll("[data-src]"); //looks for attribute
-const options = {
-    threshold: 0.5, 
-    rootMargin: "0px 0px 100px 0px"
-};
+const images = document.body.querySelectorAll('[data-src');    //Body or no body?
+
+const options = {threshold: .5,
+                rootMargin: '0px 0px 100px 0px'};
 
 function preloadImage(img){
-    const source = img.getAttribute("data-src");
-    if(!source){
-        return;
-    }
-    img.src = source;
-}
-const io = new IntersectionObserver((entries, 
-    io) => {
-        entries.forEach(entry => {
-            if(!entry.isIntersecting){
-                return;
-            } else{
-                preloadImage(entry.target);
-                io.unobserve(entry.target);
-            }
-        // console.log(entries); // list of items that are being observe. First parameter 
-    })
-}, options);
+  const source = img.getAttribute('data-src');
+  if(!source){
+    return;
+  }
+  
+  img.src = source;
 
-images.forEach(img =>{
-    io.observe(img);
-});
+  
+}
+
+const io = new IntersectionObserver (
+    (entries) => {
+        entries.forEach(entry => {
+          if(!entry.isIntersecting){
+            return;
+          }else{
+            preloadImage(entry.target);
+          }
+          
+        })
+        
+        
+    }, options
+    
+
+);
+
+images.forEach(image => {
+  io.observe(image);
+})
