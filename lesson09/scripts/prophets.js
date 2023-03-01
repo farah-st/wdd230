51e343c5151fb12a413d92da667043cad82feb81
farah-st <farah.perezcarrillo@gmail.com>
2023-03-01 12:52:22 -0700

    CHANGES

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -21,26 +21,30 @@ async function getProphetData() { //running asyncronist at least one await
       // Create elements to add to the div.cards element
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
-      
+      let p1 = document.createElement('p');
+      let p2 = document.createElement('p');
       let portrait = document.createElement('img');
       
   
       // Build the h2 content out to show the prophet's full name - finish the template string
       h2.textContent = `${prophet.name} ${prophet.lastname}`;      
-                                        
+      p1.textContent = `Date of Birth: ${prophet.birthdate}`;   
+      p2.textContent = `Place of Birth: ${prophet.birthplace}`; 
         
       // Build the image portrait by setting all the relevant attribute
       portrait.setAttribute('src', prophet.imageurl);
       portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
-      
+     
       portrait.setAttribute('loading', 'lazy');
     //   portrait.setAttribute('width', '340');
     //   portrait.setAttribute('height', '440');
   
       // Append the section(card) with the created elements
       card.appendChild(h2);
+      card.appendChild(p1);
+      card.appendChild(p2);
       card.appendChild(portrait);
-  
       cards.appendChild(card);
     } // end of forEach loop
  )} // end of function expression
+
