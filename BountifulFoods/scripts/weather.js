851e6ba8f717128e1f7139b7eacd9087c2ce46d5
farah-st <farah.perezcarrillo@gmail.com>
2023-04-01 15:56:54 -0600

    i cant any more

--- BountifulFoods/scripts/weather.js
+++ BountifulFoods/scripts/weather.js
@@ -5,7 +5,7 @@ const getWeather = async () => {
     console.log(data);
     let t = data.currentConditions.temp;
     document.querySelector('#t').textContent = t.toFixed(1);
-    // let image = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/1st%20Set%20-%20Color/${data.currentConditions.icon}.svg`;
+    let image = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/1st%20Set%20-%20Color/${data.currentConditions.icon}.svg`;
     document.querySelector("#humidity").innerHTML = data.currentConditions.humidity;
     document.querySelector('#condition').textContent =  data.currentConditions.conditions;
     document.querySelector('#weather_icon').src = image;
