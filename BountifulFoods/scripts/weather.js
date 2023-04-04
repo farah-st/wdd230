75a86ada9e0538a85744a04007ce7455e31db5bf
farah-st <farah.perezcarrillo@gmail.com>
2023-04-03 18:42:17 -0600

    weather fixed

--- BountifulFoods/scripts/weather.js
+++ BountifulFoods/scripts/weather.js
@@ -11,7 +11,18 @@ const getWeather = async () => {
     document.querySelector('#weather_icon').src = image;
     document.querySelector('#weather_icon').alt= data.currentConditions.conditions + ' icon';
 
-    document.querySelector('#date1').textContent = t.toFixed(1);
+    document.querySelector('#weather1').innerHTML = data.days[0].datetime;
+    document.querySelector("#temp0").innerHTML = data.days[0].temp;
+    document.querySelector("#minTemp0").innerHTML = data.days[0].tempmin;
+    document.querySelector("#maxTemp0").innerHTML = data.days[0].tempmax;
+    document.querySelector('#weather2').innerHTML = data.days[1].datetime;
+    document.querySelector("#temp2").innerHTML = data.days[1].temp;
+    document.querySelector("#minTemp2").innerHTML = data.days[1].tempmin;
+    document.querySelector("#maxTemp2").innerHTML = data.days[1].tempmax;
+    document.querySelector('#weather3').innerHTML = data.days[2].datetime;
+    document.querySelector("#temp3").innerHTML = data.days[2].temp;
+    document.querySelector("#minTemp3").innerHTML = data.days[2].tempmin;
+    document.querySelector("#maxTemp3").innerHTML = data.days[2].tempmax;
 
 
   };
